aa8edb6e89ef135b0b27809da5d34b492e410d6d
cphil124 <47870744+VCG-cphillips@users.noreply.github.com>
2019-09-11 21:14:54 -0500

    Fixed Typos

--- luigi_tut.py
+++ luigi_tut.py
@@ -3,7 +3,7 @@ import luigi
 class PrintNumbers(luigi.Task):
     n=luigi.IntParameter()
 
-    def requires():
+    def requires(self):
         return [] # First Task in the pipeline, so no requirements
 
     def output(self):
@@ -11,13 +11,16 @@ class PrintNumbers(luigi.Task):
     
     def run(self):
         with self.output().open('w') as f:
-            for i in range(1, sefl.n+1):
-                f.write('{}\n'.format()
+            for i in range(1, self.n+1):
+                f.write('{}\n'.format())
+
 
 class SquaredNumbers(luigi.Task):
 
+    n = luigi.IntParameter()
+
     def requires(self):
-        return [PrintNumbers)n=self.n]
+        return [PrintNumbers(n=self.n)]
     
     def output(self):
         return luigi.LocalTarget(f'squares_up_to_{self.n}.txt')
@@ -28,3 +31,6 @@ class SquaredNumbers(luigi.Task):
                 n = int(line.strip())
                 out = n * n
                 fout.write(f'{n}:{out}\n')
+
+if __name__ == '__main__':
+    luigi.run()
